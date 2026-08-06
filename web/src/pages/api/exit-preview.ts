dd69c60a900e86b6110823ec7871a2f3d6c3bb75
mtssnk <mailmtsnk@gmail.com>
2026-08-06 10:33:19 +0200

    fix: preview

--- web/src/pages/api/exit-preview.ts
+++ web/src/pages/api/exit-preview.ts
@@ -1,5 +1,7 @@
 import type { APIRoute } from 'astro'
 
+export const prerender = false
+
 export const GET: APIRoute = ({ cookies, redirect }) => {
   cookies.delete('__preview', { path: '/' })
   return redirect('/')
--- web/src/pages/api/preview.ts
+++ web/src/pages/api/preview.ts
@@ -1,5 +1,7 @@
 import type { APIRoute } from 'astro'
 
+export const prerender = false
+
 export const GET: APIRoute = ({ url, cookies, redirect }) => {
   const secret = url.searchParams.get('secret')
   const slug = url.searchParams.get('slug')
